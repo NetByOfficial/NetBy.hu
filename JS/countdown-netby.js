e4aff3be2abe80ba87707e791f57b47ad9488447
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-10-03 17:03:48 +0200

    last date modification

--- JS/countdown-netby.js
+++ JS/countdown-netby.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const IskolakezdesTime = new Date(`October 18 ${currentYear} 00:00:00`);
+const IskolakezdesTime = new Date(`October 16 ${currentYear} 11:00:00`);
 
 // Update countdown time
 function updateCountdown() {
