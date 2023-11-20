031a265ead726239dfdcb38e5489e8884eac4284
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-11-20 17:16:14 +0100

    Delete JS directory

--- JS/engedélyek.js
+++
@@ -1,9 +0,0 @@
-let értesítések_engedélyezve = false //Alap Érték
-
-function Értesítés_Engedélykérés() {
-    Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-           értesítések_engedélyezve = true
-        }
-    })
-}
